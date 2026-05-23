1a8dd6673904357ca52134caaa7b65378ebeb1c3
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2026-05-23 01:23:23 +0100

    Create virtual_cursor.rs

---
+++ src/simulator/conway/virtual_cursor.rs
@@ -0,0 +1,4 @@
+
+// TODO: Idea for an 'ambient mode', where it draws rainbow automatically
+// simulating a cursors natural movement through a selection of randomised
+// patterns. cursor has velocity and wiggles a bit and slows down and stuff
